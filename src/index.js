9b7325198aec8c36930c6a326b667f0570d139fd
grazielle-bastos <grazielle1101@gmail.com>
2023-09-02 01:22:04 -0300

    Atualiza mensagens de saudação e entrada para lidar com acentuação

--- src/index.js
+++ src/index.js
@@ -1,40 +1,44 @@
 const readlineSync = require('readline-sync');
 
-// Solicita o nome do herói ao usuário
-let nome = readlineSync.question("Olá, Herói! Digite o seu nome: ");
+    // Solicita o nome do herói ao usuário
+    console.log("Olá, Herói! Digite o seu nome: ");
 
-// Solicita a quantidade de experiência (XP) do herói ao usuário
-let experienciaInput = readlineSync.question("Digite a quantidade de experiência (XP) do herói: ");
+    // Armazena a entrada do usuário
+    const nome = readlineSync.question();
 
-// Converte a entrada de experiência para um número
-let experiencia = parseFloat(experienciaInput);
+    // Solicita a quantidade de experiência (XP) do herói ao usuário
+    console.log("Digite a quantidade de experiência (XP) do herói:");
 
-// Define os níveis de acordo com a experiência
-let niveis = [
-    "Ferro", "Bronze", "Prata", "Ouro",
-    "Platina", "Ascendente", "Imortal", "Radiante"
-];
+    // Converte a entrada de experiência para um número
+    const experienciaInput = readlineSync.question();
+    const experiencia = parseFloat(experienciaInput.replace(',', '.')); // Lida com números decimais (substitui vírgula por ponto)
 
-// Determina o nível do herói, mediante experiência
-let nivelDoHeroi = "";
+    // Define os níveis de acordo com a experiência
+    let niveis = [
+        "Ferro", "Bronze", "Prata", "Ouro",
+        "Platina", "Ascendente", "Imortal", "Radiante"
+    ];
 
-if (experiencia < 1000) {
-    nivelDoHeroi = niveis[0];
-} else if (experiencia <= 2000) {
-    nivelDoHeroi = niveis[1];
-} else if (experiencia <= 5000) {
-    nivelDoHeroi = niveis[2];
-} else if (experiencia <= 7000) {
-    nivelDoHeroi = niveis[3];
-} else if (experiencia <= 8000) {
-    nivelDoHeroi = niveis[4];
-} else if (experiencia <= 9000) {
-    nivelDoHeroi = niveis[5];
-} else if (experiencia <= 10000) {
-    nivelDoHeroi = niveis[6];
-} else {
-    nivelDoHeroi = niveis[7];
-}
+    // Determina o nível do herói, mediante experiência
+    let nivelDoHeroi = "";
 
-// Exibe o resultado para o usuário
-console.log("O Herói de nome " + nome + " está no nível de " + nivelDoHeroi + ".");
+    if (experiencia < 1000) {
+        nivelDoHeroi = niveis[0];
+    } else if (experiencia <= 2000) {
+        nivelDoHeroi = niveis[1];
+    } else if (experiencia <= 5000) {
+        nivelDoHeroi = niveis[2];
+    } else if (experiencia <= 7000) {
+        nivelDoHeroi = niveis[3];
+    } else if (experiencia <= 8000) {
+        nivelDoHeroi = niveis[4];
+    } else if (experiencia <= 9000) {
+        nivelDoHeroi = niveis[5];
+    } else if (experiencia <= 10000) {
+        nivelDoHeroi = niveis[6];
+    } else {
+        nivelDoHeroi = niveis[7];
+    }
+
+    // Exibe o resultado para o usuário
+    console.log("O Herói de nome " + nome + " está no nível de " + nivelDoHeroi + ".");
